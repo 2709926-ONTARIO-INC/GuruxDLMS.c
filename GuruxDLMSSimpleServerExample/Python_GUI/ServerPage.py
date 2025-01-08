17b532a65b5d1910bcae2ea46123f0c0309a3712
Dhairya Chheda <dhairyachheda1732@gmail.com>
2025-01-08 19:12:40 +0530

    Modified ServerPage

--- GuruxDLMSSimpleServerExample/Python_GUI/ServerPage.py
+++ GuruxDLMSSimpleServerExample/Python_GUI/ServerPage.py
@@ -1,20 +1,8 @@
-from PyQt5.QtWidgets import (
-    QMainWindow,
-    QWidget,
-    QVBoxLayout,
-    QTableWidget,
-    QApplication,
-    QHBoxLayout,
-    QHeaderView,
-    QTableWidgetItem,
-    QCheckBox,
-    QComboBox,
-    QMessageBox
-)
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTableWidget, QApplication, QHBoxLayout, QHeaderView, QTableWidgetItem, QCheckBox, QComboBox, QMessageBox
 from PyQt5.QtCore import Qt
 from utils import createLabel, open_next_page, open_previous_page, createButton
 import sys
-import json
+from json_config import SaveConfigJson
 import os
 import meter_automation
 
@@ -58,128 +46,15 @@ class ParameterPopup(QWidget):
 
         # Save button
         save_button = createButton("Save")
-        save_button.clicked.connect(lambda: self.save_to_json(input_table))
+        save_button.clicked.connect(lambda: self.save_popup_data(input_table))
 
         button_layout = QHBoxLayout()
         button_layout.setAlignment(Qt.AlignCenter)
         button_layout.addWidget(save_button)
         popup_layout.addLayout(button_layout)
 
-    def save_to_json(self, input_table):
-        meter_type_for_file_path = ""
-
-        # Function to check if input is a valid integer
-        def is_valid_input(item):
-            if item and item.text():  # Ensure the item and its text are not None or empty
-                text = item.text()
-                try:
-                    float(text)  # Try converting to a float
-                    return True
-                except ValueError:
-                    return False
-            return False
-
-        # Check if all fields have valid input
-        for row in range(5):
-            for col in range(1, 3):
-                item = input_table.item(row, col)
-                if not is_valid_input(item):
-                    QMessageBox.warning(self, "Input Error", "Please enter valid values.")
-                    return
-            
-        current_row = self.parent_table.currentRow()
-        if current_row == -1:
-            QMessageBox.warning(self, "Selection Error", "Please select a row to save the data.")
-            return
-        meter_type = self.parent_table.cellWidget(current_row, 0).currentText()
-        manufacturer = self.parent_table.item(current_row, 2).text()
-        no_of_meters = int(self.parent_table.item(current_row, 1).text()) if self.parent_table.item(current_row, 1).text() != "" else 0
-
-        # Convert Meter Type for file name
-        file_path_name = {"Single Phase": "single_phase", "Three Phase (WC)": "three_phase_wc", "Three Phase (LTCT)": "three_phase_ltct", "Three Phase (HTCT)": "three_phase_htct"}
-        if meter_type:
-            meter_type_for_file_path = file_path_name[meter_type]
-        
-        # Get the directory of the current script
-        script_directory = os.path.dirname(os.path.abspath(__file__))
-
-        # Construct the config file path
-        print(manufacturer.lower().replace(' ', '_'))
-        config_file_name = fr"Config\{manufacturer.lower().replace(' ', '_')}_{meter_type_for_file_path}_config.json"
-
-        # Prepend the full directory path
-        config_file_path = os.path.join(script_directory, config_file_name)
-
-        json_keys = ["no_of_meters", "meter_type", "manufacturer", "voltage_limits", "current_limits", "frequency_limits", "power_factor_limits", "block_energy_limits"]
-        phases = ["L1", "L2", "L3"]
-        limits = ["lower_limit", "upper_limit"]
-        
-        config_data = {}
-
-        # Loop to dynamically insert keys in config_data
-        for i, key in enumerate(json_keys):
-            # First three static keys
-            if key == "no_of_meters":
-                config_data[key] = no_of_meters
-            elif key == "meter_type":
-                config_data[key] = meter_type
-            elif key == "manufacturer":
-                config_data[key] = manufacturer
-            else:
-                # Keys about meter values
-                for j, limit in enumerate(limits):
-                    if key not in config_data:
-                        config_data[key] = {}
-                    # For Single Phase
-                    if meter_type == "Single Phase":
-                        # For current
-                        if key == "current_limits":
-                            subkeys = ["phase_current", "neutral_current"]
-                            for subkey in subkeys:
-                                if subkey not in config_data[key]:
-                                    config_data[key][subkey] = {}
-                                config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
-                        # For block load
-                        elif key == "block_energy_limits":
-                            subkeys = ["kWh_import", "kWh_export"]
-                            for subkey in subkeys:
-                                if subkey not in config_data[key]:
-                                    config_data[key][subkey] = {}
-                                config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 100)
-                        # For Signed power factor
-                        elif key == "power_factor_limits":
-                            del config_data[key]
-                            if "signed_" + key not in config_data:      
-                                config_data["signed_" + key] = {}
-                            config_data["signed_"+key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
-                        # Other keys
-                        else:
-                            config_data[key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
-                    # For Three phase
-                    else:
-                        # For frequency
-                        if key == "frequency_limits":
-                            config_data[key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
-                        # For block load
-                        elif key == "block_energy_limits":
-                            subkeys = ["kWh_import", "kVAh_lag", "kVAh_lead", "kVAh_import"]
-                            for subkey in subkeys:
-                                if subkey not in config_data[key]:
-                                    config_data[key][subkey] = {}
-                                config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 100)
-                        # Other keys
-                        else:
-                            for phase in phases:
-                                # Current Multiplier
-                                multiplier = 100000 if key == "current_limits" else 1000
-                                if phase not in config_data[key]:
-                                    config_data[key][phase] = {}
-                                config_data[key][phase][limit] = int(float(input_table.item(i-3, j+1).text()) * multiplier)
-
-        # Save the configuration to a new JSON file
-        with open(config_file_path, "w") as f:
-            json.dump(config_data, f, indent=4)
-            
+    def save_popup_data(self, input_table):
+        SaveConfigJson.save_to_json_config(self, input_table)   
         self.close()
 
 class ServerPage(QMainWindow):
@@ -202,17 +77,7 @@ class ServerPage(QMainWindow):
         self.table = QTableWidget(self)
         self.table.setColumnCount(7) 
 
-        self.table.setHorizontalHeaderLabels(
-            [
-                "Type of Meter",
-                "No. of Meters",
-                "Manufacturer",
-                "Starting Port No.",
-                "Starting Instance No.",
-                "Garbage Values",
-                "Configure Parameters"
-            ]
-        )
+        self.table.setHorizontalHeaderLabels(["Type of Meter", "No. of Meters", "Manufacturer", "Starting Port No.", "Starting Instance No.", "Garbage Values", "Configure Parameters"])
 
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch) 
         self.table.setStyleSheet(
---
+++ GuruxDLMSSimpleServerExample/Python_GUI/json_config.py
@@ -0,0 +1,118 @@
+from PyQt5.QtWidgets import QMessageBox
+import json
+import os
+
+class SaveConfigJson():
+    def save_to_json_config(self, input_table):
+            meter_type_for_file_path = ""
+
+            # Function to check if input is a valid integer
+            def is_valid_input(item):
+                if item and item.text():  
+                    text = item.text()
+                    try:
+                        float(text) 
+                        return True
+                    except ValueError:
+                        return False
+                return False
+
+            # Check if all fields have valid input
+            for row in range(5):
+                for col in range(1, 3):
+                    item = input_table.item(row, col)
+                    if not is_valid_input(item):
+                        QMessageBox.warning(self, "Input Error", "Please enter valid values.")
+                        return
+                
+            current_row = self.parent_table.currentRow()
+            if current_row == -1:
+                QMessageBox.warning(self, "Selection Error", "Please select a row to save the data.")
+                return
+            meter_type = self.parent_table.cellWidget(current_row, 0).currentText()
+            manufacturer = self.parent_table.item(current_row, 2).text()
+            no_of_meters = int(self.parent_table.item(current_row, 1).text()) if self.parent_table.item(current_row, 1).text() != "" else 0
+
+            # Convert Meter Type for file name
+            file_path_name = {"Single Phase": "single_phase", "Three Phase (WC)": "three_phase_wc", "Three Phase (LTCT)": "three_phase_ltct", "Three Phase (HTCT)": "three_phase_htct"}
+            if meter_type:
+                meter_type_for_file_path = file_path_name[meter_type]
+            
+            # Get the directory of the current script
+            script_directory = os.path.dirname(os.path.abspath(__file__))
+
+            # Construct the config file path
+            config_file_name = fr"Config\{manufacturer.lower().replace(' ', '_')}_{meter_type_for_file_path}_config.json"
+
+            # Prepend the full directory path
+            config_file_path = os.path.join(script_directory, config_file_name)
+
+            json_keys = ["no_of_meters", "meter_type", "manufacturer", "voltage_limits", "current_limits", "frequency_limits", "power_factor_limits", "block_energy_limits"]
+            phases = ["L1", "L2", "L3"]
+            limits = ["lower_limit", "upper_limit"]
+            
+            config_data = {}
+
+            # Loop to dynamically insert keys in config_data
+            for i, key in enumerate(json_keys):
+                # First three static keys
+                if key == "no_of_meters":
+                    config_data[key] = no_of_meters
+                elif key == "meter_type":
+                    config_data[key] = meter_type
+                elif key == "manufacturer":
+                    config_data[key] = manufacturer
+                else:
+                    # Keys about meter values
+                    for j, limit in enumerate(limits):
+                        if key not in config_data:
+                            config_data[key] = {}
+                        # For Single Phase
+                        if meter_type == "Single Phase":
+                            # For current
+                            if key == "current_limits":
+                                subkeys = ["phase_current", "neutral_current"]
+                                for subkey in subkeys:
+                                    if subkey not in config_data[key]:
+                                        config_data[key][subkey] = {}
+                                    config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
+                            # For block load
+                            elif key == "block_energy_limits":
+                                subkeys = ["kWh_import", "kWh_export"]
+                                for subkey in subkeys:
+                                    if subkey not in config_data[key]:
+                                        config_data[key][subkey] = {}
+                                    config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 100)
+                            # For Signed power factor
+                            elif key == "power_factor_limits":
+                                del config_data[key]
+                                if "signed_" + key not in config_data:      
+                                    config_data["signed_" + key] = {}
+                                config_data["signed_"+key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
+                            # Other keys
+                            else:
+                                config_data[key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
+                        # For Three phase
+                        else:
+                            # For frequency
+                            if key == "frequency_limits":
+                                config_data[key][limit] = int(float(input_table.item(i-3, j+1).text()) * 1000)
+                            # For block load
+                            elif key == "block_energy_limits":
+                                subkeys = ["kWh_import", "kVAh_lag", "kVAh_lead", "kVAh_import"]
+                                for subkey in subkeys:
+                                    if subkey not in config_data[key]:
+                                        config_data[key][subkey] = {}
+                                    config_data[key][subkey][limit] = int(float(input_table.item(i-3, j+1).text()) * 100)
+                            # Other keys
+                            else:
+                                for phase in phases:
+                                    # Current Multiplier
+                                    multiplier = 100000 if key == "current_limits" else 1000
+                                    if phase not in config_data[key]:
+                                        config_data[key][phase] = {}
+                                    config_data[key][phase][limit] = int(float(input_table.item(i-3, j+1).text()) * multiplier)
+
+            # Save the configuration to a new JSON file
+            with open(config_file_path, "w") as f:
+                json.dump(config_data, f, indent=4)
